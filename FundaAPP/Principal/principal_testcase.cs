20927f186a695d31d68341601e9a71111f040b57
Koketsot <koketsot@digitalsolutionfoundry.co.za>
2025-01-09 10:37:45 +0200

    Testbase and log in fixed.

--- FundaAPP/Principal/principal_testcase.cs
+++ FundaAPP/Principal/principal_testcase.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using OpenQA.Selenium;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,6 +9,39 @@ namespace FundaAPP.Principal
 {
     internal class principal_testcase
     {
+        [TestFixture]
+
+        public class Principal : Tests
+        {
+            private IWebDriver _driver;
+            [SetUp]
+
+            public void startBrowser()
+            {
+                SiteConnection();
+            }
+
+            [Test, Order(1)]
+            public void principalTest()
+            {
+                Delay(2);
+                //Test1();
+                //Test2();
+
+                Delay(2);
+
+            }
+
+            [Description("Verify Dashboard Elements Post Login")]
+
+      
+            [TearDown]
+            public void closeBrowser()
+            {
+                base.DisconnectBrowser();
+            }
+
+        }
 
     }
 }
--- FundaAPP/Testbase.cs
+++ FundaAPP/Testbase.cs
@@ -1,16 +1,67 @@
+using OpenQA.Selenium.Chrome;
+using NUnit.Framework;
+using OpenQA.Selenium;
+using System.Threading;
+
 namespace FundaAPP
 {
     public class Tests
     {
-        [SetUp]
-        public void Setup()
+
+        private ChromeOptions _chromeOptions;
+        public IWebDriver _driver;
+        private string _userName;
+        private string _password;
+
+        [OneTimeSetUp]
+        public void StartBrowser()
         {
+            _chromeOptions = new ChromeOptions();
+            _chromeOptions.AddArguments("--incognito");
+            _chromeOptions.AddArguments("--ignore-certificate-errors");
+            _driver = new ChromeDriver("C:/Users/KoketsoTinyane/source/repos/WorldOfImpact/WorldOfImpact/bin/Debug/Drivers");
         }
 
-        [Test]
-        public void Test1()
+        public IWebDriver SiteConnection()
         {
-            Assert.Pass();
+            _driver.Url = "https://fundaapp-app-qa-za.azurewebsites.net/";
+
+            _userName = "8601016828185";
+            _password = "Funda123";
+            _driver.Manage().Window.Maximize();
+            System.Threading.Thread.Sleep(4000);
+
+            IWebElement loginTextBox = _driver.FindElement(By.XPath("/html/body/div/ion-app/ion-router-outlet/div/div[3]/div/form/div/div[1]/div/input"));
+            IWebElement passwordTextBox = _driver.FindElement(By.XPath("/html/body/div/ion-app/ion-router-outlet/div/div[3]/div/form/div/div[2]/div/div/input"));
+            IWebElement loginBtn = _driver.FindElement(By.Id("gtm-login"));
+            loginTextBox.SendKeys(_userName);
+            System.Threading.Thread.Sleep(6000);
+            passwordTextBox.SendKeys(_password);
+            System.Threading.Thread.Sleep(4000);
+
+            Delay(9);
+       
+            loginBtn.Click();
+            Delay(30);
+
+            return _driver;
         }
+
+        [OneTimeTearDown]
+        public void DisconnectBrowser()
+        {
+            if (_driver != null)
+            {
+                _driver.Quit();
+                _driver.Dispose();
+                _driver = null; // Ensure it’s reset for each test
+            }
+        }
+
+        public void Delay(int delaySeconds)
+        {
+            Thread.Sleep(delaySeconds * 10000);
+        }
+
     }
 }
